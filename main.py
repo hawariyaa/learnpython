c76a34611c073bd4a78ce120d99166146130cc4e
hawariyaw paulos <hawipaul308@gmail.com>
2023-12-15 09:14:00 -0800

    learining python

---
+++ main.py
@@ -0,0 +1,37 @@
+import math
+
+print("new")
+new_one = "what"
+anotherone = "is"
+num_1 = 2.3
+print("new" + " " + new_one + " " + anotherone + " " + str(num_1) )
+print(anotherone.upper())
+print(6+14-4*16/8)
+print(10 % 3)
+
+
+print(math.floor(num_1))
+print(math.ceil(num_1))
+
+# simple calculator
+'''
+num_1 = input("enter first number :")
+num_2 = input("enter second number: ")
+result = float(num_1) + float(num_2)
+print("the answer is: " + str(result))
+
+# creating a mad-lib game
+color = input("enter a color: ")
+noun = input("enter a noun: ")
+clebrity = input("enter celebrity name: ")
+
+
+print("Roses are " + color)
+print( noun + " is blue")
+print("i love " + clebrity)
+'''
+# know lets see arrays or lists
+random_items = ["kevin", "is", 14, 19.45, True, "yap"]
+random_items.insert(0, False)
+print(random_items[0])
+
