3a4b67a40397337d0be74bcca87160085dd20bc3
hawariyaw paulos <hawipaul308@gmail.com>
2023-12-15 12:20:48 -0800

    learnpython1

--- main.py
+++ main.py
@@ -35,3 +35,24 @@ random_items = ["kevin", "is", 14, 19.45, True, "yap"]
 random_items.insert(0, False)
 print(random_items[0])
 
+#tuples tuples are list that can not be modified
+important_names = ("mike", "dane")
+print(important_names[0])
+'''
+#functions
+name = input("enter name: ")
+age = input("enter age: ")
+def info(name1 , age1):
+    print("your name is: " + name1 + " and your age is: " + str(age1))
+info(name, age)
+'''
+# using the return
+def cube(num1, num2, num3):
+    return float(num1) * float(num2) * float(num3)
+num1 = input("enter a number: ")
+num2 = input("enter a number: ")
+num3 = input("enter a number: ")
+
+value = cube(num1, num2, num3)
+print(str(value))
+
